3068136bbfa6697dd59ca2658ecfe5e72da5aa79
AmuDaDev <amunkombyani@gmail.com>
2024-10-02 19:54:46 +0200

    Added unit tests

--- order-service/src/test/java/com/baobab/bookstore/order_service/controller/OrderControllerUnitTest.java
+++ order-service/src/test/java/com/baobab/bookstore/order_service/controller/OrderControllerUnitTest.java
@@ -1,14 +1,82 @@
 package com.baobab.bookstore.order_service.controller;
 
+import static com.baobab.bookstore.order_service.testdata.TestDataFactory.*;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.baobab.bookstore.order_service.dto.CreateOrderRequest;
+import com.baobab.bookstore.order_service.dto.CreateOrderResponse;
+import com.baobab.bookstore.order_service.service.OrderService;
+import com.baobab.bookstore.order_service.service.SecurityService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * @author AmuDaDev
  * @created 02/10/2024
  */
+@WebMvcTest(OrderController.class)
 class OrderControllerUnitTest {
 
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private SecurityService securityService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        given(securityService.getLoginUserName()).willReturn("siva");
+    }
+
     @Test
-    void createOrder() {
+    void shouldCreateOrderSuccessfully() throws Exception {
+        given(orderService.createOrder(eq("amu"), any(CreateOrderRequest.class)))
+                .willReturn(new CreateOrderResponse("#1234"));
+
+        mockMvc.perform(post("/api/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createValidOrderRequest())))
+                .andExpect(status().isCreated());
+    }
+
+    @ParameterizedTest(name = "[{index}]-{0}")
+    @MethodSource("createOrderRequestProvider")
+    void shouldReturnBadRequestWhenOrderPayloadIsInvalid(CreateOrderRequest request) throws Exception {
+        given(orderService.createOrder(eq("amu"), any(CreateOrderRequest.class)))
+                .willReturn(null);
+
+        mockMvc.perform(post("/api/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    static Stream<Arguments> createOrderRequestProvider() {
+        return Stream.of(
+                arguments(named("Order with Invalid Customer", createOrderRequestWithInvalidCustomer())),
+                arguments(named("Order with Invalid Delivery Address", createOrderRequestWithInvalidDeliveryAddress())),
+                arguments(named("Order with No Items", createOrderRequestWithNoItems())));
     }
 }
