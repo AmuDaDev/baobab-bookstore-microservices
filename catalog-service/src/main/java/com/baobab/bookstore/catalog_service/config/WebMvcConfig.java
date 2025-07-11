8798263c0068b53addc681b6dec9ad61aa6540ac
AmuDaDev <amunkombyani@gmail.com>
2025-07-11 11:50:30 +0200

    Create maven spring boot project for api-gateway

---
+++ catalog-service/src/main/java/com/baobab/bookstore/catalog_service/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.baobab.bookstore.catalog_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author AmuDaDev
+ * @created 11/07/2025
+ */
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowCredentials(false);
+    }
+
+}
